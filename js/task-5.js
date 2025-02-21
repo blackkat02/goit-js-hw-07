00ddb82bf9a2ad374f447806d64310846ddedb96
Borys <boris02374@gmail.com>
2025-02-21 23:41:45 +0200

    added task-4,5,6 css

--- js/task-5.js
+++ js/task-5.js
@@ -2,14 +2,12 @@ const changeColorBtn = document.querySelector(".change-color");
 const body = document.body;
 const colorText = document.querySelector(".color");
 
-// Обробник події для зміни кольору при кліку на кнопку
 changeColorBtn.addEventListener("click", () => {
   const randomColor = getRandomHexColor();
   body.style.backgroundColor = randomColor;
   colorText.textContent = randomColor;
 });
 
-// Функція для генерації випадкового HEX-кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
